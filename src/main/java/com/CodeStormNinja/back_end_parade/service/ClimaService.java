bb194a5150386456f3dc240cba68ec8dd4f5ce82
Luiz Guilherme Mariano da Cruz <37917447+zBatataXDz@users.noreply.github.com>
2025-09-05 11:49:11 -0400

    refactor: 🛠️ Improve temperature classification logic in ClimaService

--- src/main/java/com/CodeStormNinja/back_end_parade/service/ClimaService.java
+++ src/main/java/com/CodeStormNinja/back_end_parade/service/ClimaService.java
@@ -33,10 +33,12 @@ public class ClimaService {
     public String logicaNegocio(DadosBrutos dados) {
         int temp = (int) dados.getTemperatura();
         switch (temp) {
-            case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15:
+            case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9:
+                return "Muito Frio";
+            case 10, 11, 12, 13, 14, 15:
                 return "Frio";
             case 16, 17, 18, 19, 20, 21, 22, 23, 24:
-                return "Ameno";
+                return "Agradável";
             case 25, 26, 27, 28, 29:
                 return "Quente";
             case 30, 31, 32, 33, 34, 35, 36:
