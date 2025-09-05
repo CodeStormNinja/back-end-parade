992793929fbf1298c51fe99a623b77f651809a70
Luiz Guilherme Mariano da Cruz <37917447+zBatataXDz@users.noreply.github.com>
2025-09-05 11:47:04 -0400

    refactor: 🛠️ Clean up ClimaService code and improve formatting

--- src/main/java/com/CodeStormNinja/back_end_parade/service/ClimaService.java
+++ src/main/java/com/CodeStormNinja/back_end_parade/service/ClimaService.java
@@ -5,15 +5,12 @@ import com.CodeStormNinja.back_end_parade.model.ClimaOutput;
 import com.CodeStormNinja.back_end_parade.model.DadosBrutos;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class ClimaService {
 
-
-
     // controller buscará esse
 
-    public ClimaOutput analisarStatus(ClimaInput input){
+    public ClimaOutput analisarStatus(ClimaInput input) {
         DadosBrutos dadosBrutos = buscarDadosBrutosSimulados(input);
 
         String fraseFinal = logicaNegocio(dadosBrutos);
@@ -21,22 +18,35 @@ public class ClimaService {
         return new ClimaOutput(fraseFinal);
     }
 
+    // metodo que terá o okhttp
+    // teste
 
-//    metodo que terá o okhttp
-//    teste
-
-    public DadosBrutos buscarDadosBrutosSimulados(ClimaInput input){
+    public DadosBrutos buscarDadosBrutosSimulados(ClimaInput input) {
 
         System.out.println("buscando dados para a localizção..." + input.getLocalidade());
 
         return new DadosBrutos(30.0, 25.8, 7.5);
     }
 
-
-
     // lógia de if else
 
-    public String logicaNegocio(DadosBrutos dados){
-        return"teste";
+    public String logicaNegocio(DadosBrutos dados) {
+        int temp = (int) dados.getTemperatura();
+        switch (temp) {
+            case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15:
+                return "Frio";
+            case 16, 17, 18, 19, 20, 21, 22, 23, 24:
+                return "Ameno";
+            case 25, 26, 27, 28, 29:
+                return "Quente";
+            case 30, 31, 32, 33, 34, 35, 36:
+                return "Muito Quente";
+            default:
+                if (dados.getTemperatura() >= 36) {
+                    return "Extremamente Quente";
+                } else {
+                    return "Dados inválidos";
+                }
+        }
     }
 }
